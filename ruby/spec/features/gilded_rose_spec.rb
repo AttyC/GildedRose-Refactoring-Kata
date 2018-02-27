3a49c9d02951f4e762ecb409485005aa26ca6bf6
Antonia Cronin <antoniajcronin@gmail.com>
2018-02-27 14:51:21 +0000

    test for general patterns

--- ruby/spec/features/gilded_rose_spec.rb
+++ ruby/spec/features/gilded_rose_spec.rb
@@ -1,7 +1,8 @@
 describe 'GildedRose' do
 
   describe "#update_quality" do
-    describe 'Generic products' do
+
+    describe 'General patterns' do
       it 'product reduces in quality closer to sell by date' do
         items = [Item.new(name="+5 Dexterity Vest", sell_in=10, quality=20)]
         GildedRose.new(items).update_quality()
@@ -11,6 +12,18 @@ describe 'GildedRose' do
         items = [Item.new(name="+5 Dexterity Vest", sell_in=0, quality=20)]
         GildedRose.new(items).update_quality()
         expect(items[0].quality).to eq 18
+      end
+      it 'quality of an item is never negative' do
+        items = [Item.new(name="+5 Dexterity Vest", sell_in=0, quality=0)]
+        GildedRose.new(items).update_quality()
+        expect(items[0].quality).to eq 0
+      end
+      it 'quality of any item is never over 50 (except Sulfuras)' do
+        items = [Item.new(name="Aged Brie", sell_in=50, quality=1)]
+        60.times do
+          GildedRose.new(items).update_quality()
+        end
+        expect(items[0].quality).to eq 50
       end
     end
 
