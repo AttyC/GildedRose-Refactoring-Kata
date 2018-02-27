e31e5933a467874b908100eb42c7e558746d3722
Antonia Cronin <antoniajcronin@gmail.com>
2018-02-27 14:52:13 +0000

    refactor first part of code to reduce ifs

--- ruby/lib/gilded_rose.rb
+++ ruby/lib/gilded_rose.rb
@@ -8,12 +8,9 @@ class GildedRose
 
   def update_quality()
     @items.each do |item|
-      if item.name != 'Aged Brie' and item.name != 'Backstage passes to a TAFKAL80ETC concert'
-        if item.quality > 0
-          if item.name != 'Sulfuras, Hand of Ragnaros'
-            item.quality = item.quality - 1
-          end
-        end
+      #1 if generic item with quality > 0
+      if item.name != 'Aged Brie' and item.name != 'Backstage passes to a TAFKAL80ETC concert' and item.name != 'Sulfuras, Hand of Ragnaros'
+        item.quality = item.quality - 1 if item.quality > 0
       else
         if item.quality < 50
           item.quality = item.quality + 1
