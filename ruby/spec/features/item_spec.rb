4783f1b7db8fa4bc79fb8f4feb3ffcec88675646
Antonia Cronin <antoniajcronin@gmail.com>
2018-02-28 13:10:10 +0000

    indicates describe a  method

--- ruby/spec/features/item_spec.rb
+++ ruby/spec/features/item_spec.rb
@@ -1,6 +1,6 @@
 describe 'Item' do
 
-  describe 'initialize' do
+  describe '#initialize' do
     it 'puts name, sell-in and quality in a string' do
       item = Item.new('Test product', 0, 0)
       expect(item.to_s).to eq 'Test product, 0, 0'
