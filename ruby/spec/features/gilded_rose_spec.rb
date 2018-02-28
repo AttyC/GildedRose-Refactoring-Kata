3b1a782fbf63a6aa2103f3f608909a50498d3ac8
Antonia Cronin <antoniajcronin@gmail.com>
2018-02-28 12:53:28 +0000

    add stub for gilded_rose class to gilded_rose spec

--- ruby/spec/features/gilded_rose_spec.rb
+++ ruby/spec/features/gilded_rose_spec.rb
@@ -1,29 +1,41 @@
-describe 'GildedRose' do
+describe GildedRose do
 
-  describe "#update_quality" do
+  subject(:gilded_rose) { described_class.new(items) }
+  let(:items) { double :items }
 
-  describe 'Sulfuras, Hand of Ragnaros' do
+  describe '#update_quality' do
+
+    describe 'Sulfuras, Hand of Ragnaros' do
       it 'quality is always 80' do
-        items = [Item.new(name="Sulfuras, Hand of Ragnaros", sell_in=0, quality=80)]
-        GildedRose.new(items).update_quality()
+        items = [Item.new(name='Sulfuras, Hand of Ragnaros', sell_in=0, quality=80)]
         expect(items[0].quality).to eq 80
       end
       it 'has no sell by date' do
-        items = [Item.new(name="Sulfuras, Hand of Ragnaros", sell_in=0, quality=20)]
-        GildedRose.new(items).update_quality()
+        items = [Item.new(name='Sulfuras, Hand of Ragnaros', sell_in=0, quality=20)]
+        allow(subject).to receive(:update_quality)
         expect(items[0].sell_in).to eq 0
       end
     end
 
     describe 'Aged Brie' do
       it 'Aged Brie increases in quality closer to sell by date' do
-        items = [Item.new(name="Aged Brie", sell_in=10, quality=20)]
-        GildedRose.new(items).update_quality()
-        expect(items[0].quality).to eq 21
+        items = [Item.new(name='Aged Brie', sell_in=10, quality=20)]
+        allow(subject).to receive(:update_quality)
+        expect { subject.upgrade_by(items[0], 1) }.to change{ items[0].quality }.by 1
+      end
+    end
+
+    describe 'Conjured' do
+      it 'Conjured items degrade in Quality twice as fast as normal items' do
+        items = [Item.new(name='Conjured', sell_in=10, quality=20)]
+        allow(subject).to receive(:update_quality)
+        expect { subject.degrade_by(items[0],5) }.to change{ items[0].quality }.by -5
       end
     end
   end
+
 end
+
 
 # * “Conjured” items degrade in Quality twice as fast as normal items
 
